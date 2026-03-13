b5f5be92a98346d3130c7b7203914d67b75a215f
Vithanage T.A <tharushaakasha22@gmail.com>
2026-03-13 14:23:34 +0530

    Configure CORS for production and update API URLs

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ require('dotenv').config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: ['https://quizem.netlify.app', 'http://localhost:5173'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+}));
 app.use(bodyParser.json());
 
 // Request logger
